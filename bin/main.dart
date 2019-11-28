c75bb81df87881db0f8fcc7e107106202713a731
Muhammed AbdElrhman <nerd5000@nerd.com>
2019-11-28 23:12:05 +0200

    Creating the Project

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,10 @@
 import 'package:bitcoin_ticker/run.dart';
-import 'dart:io'show stdin,stdout;
-
+import 'dart:io' show stdin, stdout;
 
 main(List<String> arguments) {
   RunApp run = RunApp();
 
   stdout.write('Enter Minutes to Repeate : ');
-  var minutes = stdin.readLineSync();
-  run.runApp(int.parse(minutes));
-  
+  int minutes = int.parse(stdin.readLineSync());
+  run.runApp(minutes);
 }
